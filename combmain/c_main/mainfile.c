82eb2cb5b2e2b200d12f5c5d7144fb0ca6ed69d5
Nabin <nabinpauudel664@gmail.com>
2022-03-22 18:55:05 +0545

    four

---
+++ combmain/c_main/mainfile.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int game_start_fuc(void)
+{
+    char welcome_start[] = "welcome to DOME \n By Ninedust";
+    printf("%s", welcome_start);
+}
+
+int main(void)
+{
+
+    game_start_fuc();
+}
